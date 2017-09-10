4d99be174c07001ae8b7fa5f10a06375b0f38dd1
unknown <shalinibajpai0310@gmail.com>
2017-09-11 00:44:53 +0530

    added arrow function json

--- src/components/que-ans.jsx
+++ src/components/que-ans.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import beautify  from 'js-beautify';
+import $ from 'jquery';
 import Highlight from 'react-highlight.js';
 import '../../node_modules/highlight.js/styles/tomorrow-night-eighties.css';
 import '../app.css';
@@ -9,22 +10,25 @@ import {data} from '../json/content';
 class QuesAnswer extends Component {
  constructor(props){
      super(props);
-     this.ques_ans = [];
      }
- componentWillMount(){
-    this.ques_ans = data[this.props.match.params.topic];
-    console.log("ques_ans ",this.ques_ans.type);
+ 
+ toggleAns= (e) => {
+     $(e.target).toggleClass('show-ans-active');
+        //$(this).parent().next().toggleClass('open', 1000);
+        $(e.target).parent().next().slideToggle('slow');
  }
 
   render() {
       const divStyle={
-          display:'block'
+          display:'none'
       }
       console.log("content loaded ",this.props.match.params);
+      let ques_ans = data[this.props.match.params.topic];
+      console.log("ques_ans ",ques_ans.type);
     return (
       <div>
-          <div className="topic-header text-center">{this.ques_ans.title}</div>
-                { this.ques_ans.type && this.ques_ans.type.map( (list , index ) => {
+          <div className="topic-header text-center">{ques_ans.title}</div>
+                { ques_ans.type && ques_ans.type.map( (list , index ) => {
                     return (
                         <div className="question-set-section" key={index}>  
                             <div className="console-question">{index+1}. What shows in the console ?</div>
@@ -34,8 +38,9 @@ class QuesAnswer extends Component {
                                 </Highlight>
                             </div>
                              <div className="answer-section">
-                                <div className="show-answer-text">Show Answer</div>
+                                <div className="show-answer-text" onClick={this.toggleAns}>Show Answer</div>
                             </div>
+                            <div className="show-answer" style={divStyle}>undefined</div>
                         </div>
                     )
                 }) }
--- src/json/content.js
+++ src/json/content.js
@@ -23,15 +23,73 @@ export const data = {
     let_const:{
                                 title:'let, const and Block Scoping',
                                 type:[{
-                                    question:"'use strict';console.log(productId);var productId = 12;",
-                                    answer:'undefined'
+                                        question:"'use strict';console.log(productId);var productId = 12;",
+                                        answer:'undefined'
                                     },{
-                                    question:"'use strict';console.log(productId);var productId = 12;",
-                                    answer:'undefined'
+                                        question:"'use strict';console.log(productId);let productId = 12;",
+                                        answer:'ReferenceError: productId is not defined'
                                     },{
-                                    question:"'use strict';console.log(productId);var productId = 12;",
-                                    answer:'undefined'
-                                 }]
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                        answer:'12'
+                                    },{
+                                        question:"'use strict';let productId = 12;console.log(productId);",
+                                            answer:'12'
+                                }]
         
-                             }
+                    },
+    arrow_func : {
+        title : 'Arrow Functions',
+        type : [{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        },{
+            question : "'use strict';var getPrice = () => 5.99;console.log(typeof getPrice);",
+            answer : "function"
+        }]
+    }
 }
