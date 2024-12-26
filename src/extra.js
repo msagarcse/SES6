35c1b48a206aead5fd6af3962bf69b7976b6c1d4
msagarcse <msagarphd@outlook.com>
2024-12-26 22:40:07 +0530

    Update extra.js
    4th

--- src/extra.js
+++ src/extra.js
@@ -1,3 +1,9 @@
 console.log("Hello..............master............this are my additions..........");
 console.log("This changes from GitHub.............");
 console.log("not new");
+
+
+//Object creation in ES6
+
+const student ={};
+console.log(student);
